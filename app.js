196df0350333a04e4719207dfef0aa570c87bbe5
aiyushbedijavarnd <aiyushbedijavarnd@gmail.com>
2021-09-21 23:12:16 +0530

    updated app

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const donorRoutes = require('./routes/donorRoutes');
-const authRoutes = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser } = require('./middleware/authMiddleware');
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
+const enquiryRoutes = require('./routes/enquiryRoutes');
 
 // express app
 const app = express();
@@ -19,27 +13,10 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .then(result => app.listen(3000))
   .catch(err => console.log(err));
 
-
-//cors setup
-var corsOptions = {
-  origin: "http://localhost:4200"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
 // register view engine
 app.set('view engine', 'ejs');
-
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.json());
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
@@ -47,21 +24,20 @@ app.use((req, res, next) => {
   next();
 });
 
-// routes
-app.get('*', checkUser);
 
+// routes
 app.get('/', (req, res) => {
   res.render('home', { title: 'About' });
 });
 
-//Authentication routes
-app.use(authRoutes);
-  
-// Application routes
-app.use('/donors',requireAuth, donorRoutes);
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
 
+// Enquiry routes
+app.use('/enquirys', enquiryRoutes);
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
+  res.status(404).render('404', { title: '404' });
 });
